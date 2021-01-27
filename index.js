2b32e0a712c6c0fa37d4c08f9ccbe26fc671cbcb
Juan Mrad <juansmrad@gmail.com>
2021-01-26 20:13:15 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var owners = [
+    {
+        id: 1,
+        name: "Adam",
+        pets: [
+            {
+                id: 1,
+                name: "Vera",
+                type: "Dog"
+            },
+            {
+                id: 2,
+                name: "Felix",
+                type: "Cat"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Kamilah",
+        pets: [
+            {
+                id: 1,
+                name: "Doug",
+                type: "Dog"
+            }
+        ]
+    }
+];
+
+
+// GET /api/owners
+
+// GET /api/owners/:id
+
+// POST /api/owners
+
+// PUT /api/owners/:id
+
+// DELETE /api/owners/:id
+
+// GET /api/owners/:id/pets
+
+// GET /api/owners/:id/pets/:petId
+
+// POST /api/owners/:id/pets
+
+// PUT /api/owners/:id/pets/:petId
+
+// DELETE /api/owners/:id/pets/:petId
+
+
+app.listen(3000, function(){
+    console.log('Pets API is now listening on port 3000...');
+})
